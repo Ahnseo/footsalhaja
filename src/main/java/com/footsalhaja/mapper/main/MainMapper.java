037802d11ac8398df844f13b608d62f7dbefc7e2
suj00033 <suj00033@naver.com>
2022-12-05 14:28:01 +0900

    insert, list, bookDto
    1205

--- src/main/java/com/footsalhaja/mapper/main/MainMapper.java
+++ src/main/java/com/footsalhaja/mapper/main/MainMapper.java
@@ -2,16 +2,22 @@ package com.footsalhaja.mapper.main;
 
 import java.util.List;
 
+import com.footsalhaja.domain.main.BookDto;
 import com.footsalhaja.domain.main.MainDto;
 
 public interface MainMapper {
 
-	int insert(MainDto id);
+	int insert(MainDto book);
 	
-	List<MainDto> list(int offset, int records, String type, String keyword);
-
 	MainDto getById(int bookId);
 
 	int update(MainDto main);
 	
+	String selectNick(String userId) ;
+
+	List<BookDto> listBook();
+	
+	BookDto select(int bookId, String nickName);
+	
 }
+ 
