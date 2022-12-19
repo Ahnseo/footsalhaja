fe395f13fcf69fd6f895c02972424092f7a213b4
ghlnh <110441894+ghlnh@users.noreply.github.com>
2022-12-19 14:41:40 +0900

    충돌 해결

--- src/main/java/com/footsalhaja/controller/member/MemberController.java
+++ src/main/java/com/footsalhaja/controller/member/MemberController.java
@@ -1,15 +1,11 @@
 package com.footsalhaja.controller.member;
 
-<<<<<<< HEAD
-=======
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
->>>>>>> main
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -112,10 +108,6 @@ public class MemberController {
 	public void getAndModify(@RequestParam(name="userId") String userId, Model model){
 		//RequestParam 으로 member/get?userId= 아이디값 가져와서 db 요청 -> MemberDto 타입 member ->  addAttribute "member" 넣음 . 
 		//System.out.println(userId);
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 		MemberDto memberInfoByUserId = (MemberDto) memberService.selectMemberInfoByUserId(userId).get(0);
 		System.out.println(memberInfoByUserId);
 
--- src/main/java/com/footsalhaja/service/member/MemberServiceImpl.java
+++ src/main/java/com/footsalhaja/service/member/MemberServiceImpl.java
@@ -10,16 +10,10 @@ import org.apache.catalina.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-<<<<<<< HEAD
-import com.footsalhaja.domain.academy.BoardDto;
-=======
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-
->>>>>>> main
+import com.footsalhaja.domain.academy.BoardDto;
 import com.footsalhaja.domain.member.MemberDto;
 import com.footsalhaja.domain.member.MemberPageInfo;
 import com.footsalhaja.mapper.member.MemberMapper;
@@ -37,10 +31,6 @@ public class MemberServiceImpl implements MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
 	
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 	@Autowired
 	private S3Client s3Client;
 	
@@ -48,9 +38,6 @@ public class MemberServiceImpl implements MemberService {
 	private String bucketName;
 	
 	
-<<<<<<< HEAD
-=======
-
 	@Override
 	public MemberDto getByEmail(String email) {
 		return memberMapper.selectByEmail(email);
@@ -66,8 +53,7 @@ public class MemberServiceImpl implements MemberService {
 	public MemberDto getById(String userId){
 		return memberMapper.selectByUserId(userId);
 	}
-
->>>>>>> main
+	
 	//회원가입(등록) 또는 회원정보 수정
 	@Override
 	public int insertMember(MemberDto member) {
@@ -116,19 +102,11 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public List<Object> selectMemberInfoByUserId(String userId) {
 		List<Object> list = new ArrayList<>();
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 		
 		MemberDto memberInfo = memberMapper.selectMemberInfoByUserId(userId);
 		
 		Map<String, Integer> countActivity = new HashMap<>();
 		
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 		int countAllAblist = memberMapper.countAllAblist(userId);
 		int countAllFblist = memberMapper.countAllFblist(userId);
 		int countAllMainlist = memberMapper.countAllMainlist(userId);
@@ -157,7 +135,6 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public int deleteMemberInfoByUserId(String userId) {
 		//프로필 이미지 삭제
-<<<<<<< HEAD
 		String profileImg = memberMapper.selectMemberInfoByUserId(userId).getProfileImg();
 		
 		if (profileImg != null) {
@@ -174,40 +151,6 @@ public class MemberServiceImpl implements MemberService {
 		memberMapper.deleteMemberReplysByUserId(userId);
 		//회원탈퇴 좋아요 지우기
 		memberMapper.deleteMemberLikesByUserId(userId);
-=======
-
-	
-		//저장된 파일의 경로 지정
-		String path = "user_profile/" +userId;
-
-		File folder = new File(path);
-		
-		File[] listFiles = folder.listFiles();
-
-		if (listFiles != null) {
-				for (File file : listFiles) {
-
-					deleteFile(userId, file.getName());
-
-				}
-			}
-		
-		for (File file: listFiles) {
-			file.delete();
-		}
-		folder.delete();
-		
-		memberMapper.deleteProfileImgByUserId(userId);
-		
-
-		//회원탈퇴 게시물 댓글 지우기
-		
-		//회원탈퇴 게시물 지우기
-		
-		//회원탈퇴 좋아요 지우기
-
-		
->>>>>>> main
 		//회원탈퇴 FK Authority ByUserId
 		memberMapper.deleteAuthorityByUserId(userId);
 		//회원탈퇴 member ByUserId
@@ -252,10 +195,7 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public int updateMemberInfoByUserId(MemberDto memberModifiedValues, MultipartFile file) {
 		
-<<<<<<< HEAD
 		
-=======
->>>>>>> main
 		if (file != null && file.getSize() > 0) {
 			//기존 프로필 삭제
 			memberMapper.deleteProfileImgByUserId(memberModifiedValues.getUserId());
@@ -264,10 +204,6 @@ public class MemberServiceImpl implements MemberService {
 			memberMapper.insertprofileImg(memberModifiedValues.getUserId(), file.getOriginalFilename());
 			// 파일 저장
 			uploadFile(memberModifiedValues.getUserId(),file);
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 		}
 		
 		int cnt = memberMapper.updateMemberInfoByUserId(memberModifiedValues);
